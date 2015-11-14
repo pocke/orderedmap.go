a154686c75f6fdc98a424a84bcb140f38ee9011a
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-14 19:20:13 +0900

    WIP json

--- orderedmap.go
+++ orderedmap.go
@@ -1,5 +1,10 @@
 package omap
 
+import (
+	"bytes"
+	"encoding/json"
+)
+
 type any interface{}
 
 type Map interface {
@@ -8,6 +13,9 @@ type Map interface {
 	Delete(key string)
 
 	Each(func(any))
+
+	MarshalJSON() ([]byte, error)
+	UnmarshalJSON([]byte) error
 }
 
 type omap struct {
@@ -16,6 +24,8 @@ type omap struct {
 }
 
 var _ Map = &omap{}
+var _ json.Marshaler = &omap{}
+var _ json.Unmarshaler = &omap{}
 
 func New() Map {
 	return &omap{
@@ -60,6 +70,93 @@ func (m *omap) Each(f func(any)) {
 	}
 }
 
-func MarshalJSON(b []byte) error {
+func (m *omap) MarshalJSON() ([]byte, error) {
+	buf := bytes.NewBuffer([]byte{'{'})
+
+	for _, key := range m.keys {
+		// Marshal key
+		b, err := json.Marshal(key)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(b)
+		buf.WriteByte(':')
+
+		// Marshal value
+		b, err = json.Marshal(m.body[key])
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(b)
+		buf.WriteByte(',')
+	}
+
+	b := buf.Bytes()
+	b[len(b)-1] = '}'
+	return b, nil
+}
+
+func (m *omap) UnmarshalJSON(b []byte) error {
+	err := json.Unmarshal(b, &m.body)
+	if err != nil {
+		return err
+	}
+
+	dec := json.NewDecoder(bytes.NewReader(b))
+
+	dec.Token() // {
+	for dec.More() {
+		t, err := dec.Token()
+		if err != nil {
+			return err
+		}
+
+		m.keys = append(m.keys, t.(string))
+
+		t, err = dec.Token()
+		if err != nil {
+			return err
+		}
+		switch tok := t.(type) {
+		case json.Delim:
+			err := skipDelim(dec, tok)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	dec.Token() // }
+
+	return nil
+}
+
+func skipDelim(dec *json.Decoder, start json.Delim) error {
+	var end json.Delim
+	if start == '[' {
+		end = ']'
+	} else { // '{'
+		end = '}'
+	}
+
+	cnt := 1
+	for {
+		t, err := dec.Token()
+		if err != nil {
+			return err
+		}
+		delim, isDelim := t.(json.Delim)
+		if !isDelim {
+			continue
+		}
+		switch delim {
+		case start:
+			cnt++
+		case end:
+			cnt--
+			if cnt == 0 {
+				return nil
+			}
+		}
+	}
 	return nil
 }
