f412f01b4705e360b3b7e6e678cb872771c41f7e
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-14 15:04:41 +0900

    key is a string

--- orderedmap.go
+++ orderedmap.go
@@ -3,38 +3,40 @@ package omap
 type any interface{}
 
 type Map interface {
-	Set(key, value any)
-	Get(key any) (any, bool)
-	Delete(key any)
+	Set(key string, value any)
+	Get(key string) (any, bool)
+	Delete(key string)
+
+	Each(func(any))
 }
 
 type omap struct {
-	keys []any
-	body map[any]any
+	keys []string
+	body map[string]any
 }
 
 var _ Map = &omap{}
 
 func New() Map {
 	return &omap{
-		keys: make([]any, 0),
-		body: make(map[any]any),
+		keys: make([]string, 0),
+		body: make(map[string]any),
 	}
 }
 
-func (m *omap) Set(key, value any) {
+func (m *omap) Set(key string, value any) {
 	if _, exist := m.body[key]; !exist {
 		m.keys = append(m.keys, key)
 	}
 	m.body[key] = value
 }
 
-func (m *omap) Get(key any) (any, bool) {
+func (m *omap) Get(key string) (any, bool) {
 	v, ok := m.body[key]
 	return v, ok
 }
 
-func (m *omap) Delete(key any) {
+func (m *omap) Delete(key string) {
 	i := index(m.keys, key)
 	if i == -1 {
 		return
@@ -43,11 +45,21 @@ func (m *omap) Delete(key any) {
 	m.keys = append(m.keys[:i], m.keys[i+1:]...)
 }
 
-func index(keys []any, key any) int {
+func index(keys []string, key string) int {
 	for i, k := range keys {
 		if k == key {
 			return i
 		}
 	}
 	return -1
+}
+
+func (m *omap) Each(f func(any)) {
+	for _, v := range m.keys {
+		f(m.body[v])
+	}
+}
+
+func MarshalJSON(b []byte) error {
+	return nil
 }
